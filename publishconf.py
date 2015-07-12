e7789c6be1c25369dcf703582db08752d107ca71
Senthil Kumaran S <stylesen@gmail.com>
2015-07-12 21:24:36 +0530

    Remove trailing slash from SITE_URL in publish conf.

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'http://geekstart.tce.edu/'
+SITEURL = 'http://geekstart.tce.edu'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
